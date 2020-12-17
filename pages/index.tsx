17558e4215c3e5391c36ce1f828d013c42b096bf
jakub <kijekkuba2006@gmail.com>
2020-12-17 12:08:33 +0100

    adding dashboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,66 @@
+import Post from '@models/Post';
+import connectDatabase from '@utils/connectDatabase';
+import { GetStaticProps } from 'next';
 import React from 'react';
+import Link from 'next/link';
 
-const IndexPage: React.FC = () => (
-  <div>
-    <h1 className='text'>
-      index page
-    </h1>
+interface PostData {
+  header: string;
+  path: string;
+}
+interface Props {
+  posts: PostData[];
+}
+
+const IndexPage: React.FC<Props> = ({ posts }) => (
+  <div className='page index-page'>
+    <Link href='/post/new'>create new post</Link>
+    <br/><br/><br/><br/>
+
+    {posts && posts.map((post, index) => (
+        <article className='post' key={index}>
+          <header>
+            {post.header}
+          </header>
+          <Link href={`/post/${post.path}`}>
+            go to
+          </Link>
+          <br/>
+          <Link href={`/post/${post.path}/edit`}>
+            edit
+          </Link>
+          <br/><br/><br/><br/>
+        </article>
+    ))}
   </div>
 );
+
+export const getStaticProps: GetStaticProps = async () => {
+  await connectDatabase()
+
+  try {
+    const response = await Post.find({});
+    const posts = response.reduce((posts, post) => [...posts, {
+      header: post.header,
+      path: post.path,
+    }], []);
+
+    console.log({ response, posts });
+
+    return {
+      props: {
+        posts,
+      },
+      revalidate: 60,
+    }
+  } catch(error) {
+    console.log(error);
+    return {
+      props: {
+        posts: [],
+      },
+    }
+  }
+}
 
 export default IndexPage;
